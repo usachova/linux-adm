b20adfc885196628723151a454bb22c3ca591bd1
usachova <55155963+usachova@users.noreply.github.com>
2021-10-01 19:07:30 +0300

    1-2

--- lab1/lab1.sh
+++ lab1/lab1.sh
@@ -1,2 +1,17 @@
 #!/bin/sh
-;;;;
+
+# 1. создать каталог test в домашнем каталоге пользователя
+mkdir ~/test
+
+# 2. создать в нем файл list, содержащий список всех файлов и поддиректориев каталога /etc
+cd ~/test
+
+for f in /etc/*
+do
+  if [[ -d "$f" ]] 
+  then
+    echo "/$f" >> list
+   else
+    echo "$f" >> list
+  fi
+done;
