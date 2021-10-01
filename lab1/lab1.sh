95ed7fc62001e12d5fce0c455ed3834b7c445c23
usachova <55155963+usachova@users.noreply.github.com>
2021-10-01 19:23:15 +0300

    2-3

--- lab1/lab1.sh
+++ lab1/lab1.sh
@@ -1,30 +1,36 @@
 #!/bin/sh
 
 rm -rf ~/test
+rm -r ~/list
 
 # 1. создать каталог test в домашнем каталоге пользователя
 mkdir ~/test
 
 # 2. создать в нем файл list, содержащий список всех файлов и поддиректориев каталога /etc
 cd ~/test
-
+cnt_dirs = 0
+cnt_hfiles = 0
 for f in /etc/*
 do
   if [[ -d "$f" ]] 
   then
     echo "dir: $f" >> list
+    let cnt_dirs = cnt_dirs + 1
    else
     echo "file: $f" >> list
   fi
 done;
-
-
 for f in /etc/.*
 do
-  if [[ -d "$f" ]] 
+  if [[ -d "$f" && ("$f" != "/etc/." && "$f" != "/etc/..") ]] 
   then
     echo "dir: $f" >> list
    else
     echo "file: $f" >> list
+    cnt_hfiles = cnt_hfiles + 1
   fi
 done;
+
+# 3. вывести в конец этого файла два числа
+echo "cnt dirs: $cnt_dirs" >> list
+echo "cnt hfiles: $cnt_hfiles" >> list
