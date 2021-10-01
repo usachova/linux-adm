4f6bb654bac657d30880cbfd09063a9e86d34153
usachova <55155963+usachova@users.noreply.github.com>
2021-10-01 20:01:42 +0300

    12

--- lab1/lab1.sh
+++ lab1/lab1.sh
@@ -37,3 +37,39 @@ done;
 # 3. вывести в конец этого файла два числа
 echo "cnt dirs: $cnt_dirs" >> list
 echo "cnt hfiles: $cnt_hfiles" >> list
+
+# 4. создать в каталоге test каталог links
+mkdir ~/test/links
+
+# 5. создать в каталоге links жесткую ссылку на файл list с именем list_hlink
+ln ~/test/list ~/test/links/list_hlink
+
+# 6. создать в каталоге links символическую ссылку на файл list с именем list_slink
+ln -s ~/test/list ~/test/links/list_slink
+
+# 7. вывести на экран количество..
+cnt_hlink = $(ls -l ~/test/links/list_hlink | cut -d " " -f2)
+cnt_list = $(ls -l ~/test/list | cut -d " " -f2)
+cnt_slink = $(ls -l ~/test/links/list_slink | cut -d " " -f2)
+echo $cnt_hlink $cnt_list $cnt_slink
+
+# 8. дописать в конец файла list_hlink число строк в файле list
+wc -l ~/test/list >> ~/test/links/list_hlink
+
+# 9. сравнить содержимое файлов list_hlink и list_slink
+if cmp ~/test/links/list_hlink ~/test/links/list_slink 
+then
+	echo "YES"
+fi
+
+# 10. Переименовать файл list в list1
+mv ~/test/list ~/test/list1
+
+# 11. сравнить содержимое файлов list_hlink и list_slink
+if cmp ~/test/links/list_hlink ~/test/links/list_slink 
+then
+	echo "YES"
+fi
+
+# 12. cоздать в домашнем каталоге пользователя жесткую ссылку на каталог links
+ln ~/test/links ~/hlink
