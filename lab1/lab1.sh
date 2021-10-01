08c7fe12f7415b08db7e84c22d7e79d8fd631ee7
usachova <55155963+usachova@users.noreply.github.com>
2021-10-01 21:19:28 +0300

    30

--- lab1/lab1.sh
+++ lab1/lab1.sh
@@ -52,29 +52,32 @@ ln -s ~/test/list ~/test/links/list_slink
 cnt_hlink=$(ls -l ~/test/links/list_hlink | cut -d " " -f2)
 cnt_list=$(ls -l ~/test/list | cut -d " " -f2)
 cnt_slink=$(ls -l ~/test/links/list_slink | cut -d " " -f2)
-echo $cnt_hlink $cnt_list $cnt_slink
+echo "7. " $cnt_hlink $cnt_list $cnt_slink
 
 # 8. дописать в конец файла list_hlink число строк в файле list
 wc -l ~/test/list | cut -d " " -f1 >> ~/test/links/list_hlink
 
 # 9. сравнить содержимое файлов list_hlink и list_slink
-if diff -q ~/test/links/list_hlink ~/test/links/list_slink 
+if cmp ~/test/links/list_hlink ~/test/links/list_slink 
 then
-	echo "YES"
+  echo "9. YES"
+else
+  echo "9. NO"
 fi
 
 # 10. Переименовать файл list в list1
 mv ~/test/list ~/test/list1
 
 # 11. сравнить содержимое файлов list_hlink и list_slink
-if diff -q ~/test/links/list_hlink ~/test/links/list_slink 
+if cmp ~/test/links/list_hlink ~/test/links/list_slink 
 then
-	echo "YES"
+  echo "11. YES"
+else
+  echo "11. NO"
 fi
 
 # 12. cоздать в домашнем каталоге пользователя жесткую ссылку на каталог links
-# ln ~/test/links ~/hlink
-ln -s ~/test/links ~/hlink
+ln ~/test/links ~/hlink
 
 # 13. cоздать в домашнем каталоге файл list_conf, содержащий список файлов с расширением .conf, из каталога /etc и всех его подкаталогов
 find /etc/ -type f -name "*.conf" > ~/list_conf
@@ -99,3 +102,43 @@ find ~/test
 
 # 20. создать в домашнем каталоге файл man.txt, содержащий документацию на команду man
 man man > ~/man.txt
+
+# 21. разбить файл man.txt на несколько файлов, каждый из которых будет иметь размер не более 1 килобайта
+split -b 1K ~/man.txt man_copy_
+
+# 22. создать каталоге test каталог man.dir
+mkdir ~/test/man.dir
+
+# 23. переместить одной командой все файлы, полученные в пункте 21 в каталог man.dir
+mv man_copy_* ~/test/man.dir
+
+# 24. собрать файлы в каталоге man.dir обратно в файл с именем man.txt
+cat ~/test/man.dir/man_copy_* > ~/test/man.dir/man.txt
+
+# 25. сравнить файлы man.txt в домашней каталоге и в каталоге man.dir и вывести YES, если файлы идентичны
+if cmp ~/man.txt ~/test/man.dir/man.txt 
+then
+  echo "25. YES"
+else
+  echo "25. NO"
+fi
+
+# 26. добавить в файл man.txt, находящийся в домашнем каталоге несколько строчек с произвольными символами в начало файла и несколько строчек в конце файла
+echo "smth1234\n5678\n910\n$(cat ~/man,txt)\n0-0\n0101010\n" > ~/man.txt
+
+# 27. одной командой получить разницу между файлами в отдельный файл в стандартном формате для наложения патчей
+diff -u ~/man.txt ~/test/man.dir/man.txt > ~/man_patch.txt
+
+# 28. переместить файл с разницей в каталог man.dir
+mv ~/man_patch.txt ~/test/man.dir
+
+# 29. наложить патч из файла с разницей на man.txt в каталоге man.dir
+patch ~/test/man.dir/man.txt ~/test/man.dir/man_patch.txt
+
+# 30. Сравнить файлы man.txt в домашней каталоге и в каталоге man.dir и вывести YES, если файлы идентичны.
+if cmp ~/man.txt ~/test/man.dir/man.txt 
+then
+  echo "30. YES"
+else
+  echo "30. NO"
+fi
